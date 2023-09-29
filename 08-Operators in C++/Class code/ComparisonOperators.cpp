d216e8559f1a0510e74cd402022a99fe37f3b16a
Omprakash <omprakash8578502@gmail.com>
2023-09-29 19:04:15 +0530

    Source code for operators

---
+++ 08-Operators in C++/Class code/ComparisonOperators.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the value of a: ";
+    cin>>a;
+    cout<<"Enter the value of b: ";
+    cin>>b;
+
+    //==
+    // if(a==b)
+    //     cout<<"yes";
+    // else
+    //     cout<<"no";
+
+    // >
+    // if(a>b)
+    //     cout<<"yes";
+    // else
+    //     cout<<"no";
+
+    // <
+    // if(a<b)
+    //     cout<<"yes";
+    // else
+    //     cout<<"no";
+
+    // <=
+    // if(a<=b)
+    //     cout<<"yes";
+    // else
+    //     cout<<"no";
+
+    // >=
+    // if(a>=b)
+    //     cout<<"yes";
+    // else
+    //     cout<<"no";
+    
+    // !=
+    if(a!=b)
+        cout<<"yes";
+    else
+        cout<<"no";
+}
---
+++ 08-Operators in C++/Class code/LogicalOperators.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int a,b,c;
+    // cout<<"Enter the value of a: ";
+    // cin>>a;
+    // cout<<"Enter the value of b: ";
+    // cin>>b;
+    // cout<<"Enter the value of c: ";
+    // cin>>c;
+
+// &&
+    // if(a>b && a>c)
+    //     cout<<"yes";
+    // else
+    //     cout<<"no";
+
+
+// ||
+    // char name ='b';
+    // if(name=='a' || name=='e' || name=='i' || name=='o' || name=='u' || name=='A' || 
+    //     name=='E' || name=='I' || name=='O' || name=='U'){
+    //     cout<<"Vowel";
+    // }else{
+    //     cout<<"Consonant";
+    // }
+
+// !
+        cout<<!20;//0
+    
+
+
+
+}
